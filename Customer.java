d2bffdd8abd8bfecd6f3274771b87fa3dd9f37bb
JoseMonteiroSantos <josefms564@gmail.com>
2022-09-11 08:51:26 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -1,8 +1,11 @@
 import java.util.Enumeration;
 import java.util.Vector;
+
 public class Customer {
+
    private String _name;
    private Vector _rentals = new Vector();
+
    public Customer (String name){
       _name = name;
    }
@@ -22,54 +25,47 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+     
         thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
-@@ -61,4 +46,28 @@ public String statement() {
+        // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            each.getDaysRented() > 1) frequentRenterPoints ++;
+        //show figures for this rental
+        result += "\t" + each.getMovie().getTitle()+ "\t" +
+            String.valueOf(thisAmount) + "\n";
+        totalAmount += thisAmount;
+     }
+     //add footer lines
+     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
+     result += "You earned " + String.valueOf(frequentRenterPoints) +
              " frequent renter points";
      return result;
    }
-}
 
    private double amountFor(Rental each) {
-      //determine amounts for each line
+    //determine amounts for each line
 
-      double thisAmount = 0;
+    double thisAmount = 0;
 
-      switch (each.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (each.getDaysRented() > 2)
-               thisAmount += (each.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-               thisAmount += (each.getDaysRented() - 3) * 1.5;
-             break;
-      }
+    switch (each.getMovie().getPriceCode()) {
+       case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2)
+             thisAmount += (each.getDaysRented() - 2) * 1.5;
+          break;
+       case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;
+       case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3)
+             thisAmount += (each.getDaysRented() - 3) * 1.5;
+           break;
+    }
 
-      return thisAmount;
-   }
+    return thisAmount;
+ }
 }
