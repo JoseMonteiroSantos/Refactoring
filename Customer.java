89276e1b3561a040a31a54db79eaa32c87e0e31f
JoseMonteiroSantos <josefms564@gmail.com>
2022-09-11 09:02:27 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -26,12 +26,12 @@ public class Customer {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
      
-        thisAmount = amountFor(each);
+        thisAmount = each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
-        if ((1.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
             each.getDaysRented() > 1) frequentRenterPoints ++;
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
@@ -45,27 +45,4 @@ public class Customer {
      return result;
    }
 
-   private double amountFor(Rental aRental) {
-    //determine amounts for each line
-
-    double thisAmount = 0;
-
-    switch (aRental.getMovie().getPriceCode()) {
-       case Movie.REGULAR:
-          thisAmount += 2;
-          if (aRental.getDaysRented() > 2)
-             thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-          break;
-       case Movie.NEW_RELEASE:
-          thisAmount += aRental.getDaysRented() * 3;
-          break;
-       case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (aRental.getDaysRented() > 3)
-             thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-           break;
-    }
-
-    return thisAmount;
- }
 }
